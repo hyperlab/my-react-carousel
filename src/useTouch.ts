8d9b1f8848a0860aa0f21237cc3dbb9842c12260
Alfred Ringstad <alfred.ringstad@hyperlab.se>
2019-07-10 11:31:50 +0200

    Decrease re-renders of useTouch hook

--- src/useTouch.ts
+++ src/useTouch.ts
@@ -1,14 +1,25 @@
 import { default as React } from "react";
 
 function useTouch(callback: (offset: number) => void) {
-  const [touchStartX, setTouchStartX] = React.useState(null);
-  const [touchStartY, setTouchStartY] = React.useState(null);
+  const isSwiping = React.useRef(false);
+  const touchStartX = React.useRef(null);
+  const touchStartY = React.useRef(null);
   const [touchOffset, setTouchOffset] = React.useState(0);
-  const [isSwiping, setIsSwiping] = React.useState(false);
+
+  const reset = () => {
+    setTimeout(() => setTouchOffset(0), 0);
+    touchStartX.current = null;
+    touchStartY.current = null;
+    isSwiping.current = false;
+  };
 
   React.useEffect(() => {
     const preventDefault = (event: TouchEvent) => {
-      if (isSwiping && event.cancelable) {
+      if (
+        event.changedTouches.length === 1 &&
+        isSwiping.current &&
+        event.cancelable
+      ) {
         event.preventDefault();
         event.returnValue = false;
         return false;
@@ -39,15 +50,22 @@ function useTouch(callback: (offset: number) => void) {
         ? (event as React.TouchEvent).changedTouches[0].clientY
         : (event as React.MouseEvent).clientY;
 
-      setTouchStartX(x);
-      setTouchStartY(y);
+      touchStartX.current = x;
+      touchStartY.current = y;
     },
     []
   );
 
   const onTouchMove = React.useCallback(
     (event: React.TouchEvent | React.MouseEvent) => {
-      if (touchStartX !== null) {
+      if (touchStartX.current !== null) {
+        if (
+          (event as React.TouchEvent).changedTouches &&
+          (event as React.TouchEvent).changedTouches.length > 1
+        ) {
+          reset();
+        }
+
         const x = (event as React.TouchEvent).changedTouches
           ? (event as React.TouchEvent).changedTouches[0].clientX
           : (event as React.MouseEvent).clientX;
@@ -55,29 +73,25 @@ function useTouch(callback: (offset: number) => void) {
           ? (event as React.TouchEvent).changedTouches[0].clientY
           : (event as React.MouseEvent).clientY;
 
-        const diffX = x - touchStartX;
-        const diffY = y - touchStartY;
+        const diffX = x - touchStartX.current;
+        const diffY = y - touchStartY.current;
 
         setTouchOffset(diffX);
-        if (!isSwiping) {
+        if (!isSwiping.current) {
           if (Math.abs(diffY) > 10) {
-            setTouchStartX(null);
-            setTouchStartY(null);
-            setTouchOffset(0);
+            reset();
           } else {
-            setIsSwiping(Math.abs(diffX) > 5);
+            isSwiping.current = Math.abs(diffX) > 5;
           }
         }
       }
     },
-    [touchStartX, isSwiping]
+    [touchStartX, touchStartY, isSwiping]
   );
 
   const onTouchEnd = React.useCallback(() => {
-    callback(touchOffset);
-    setTouchStartX(null);
-    setIsSwiping(false);
-    setTimeout(() => setTouchOffset(0), 0);
+    if (isSwiping.current) callback(touchOffset);
+    reset();
   }, [touchOffset, callback]);
 
   const onClick = React.useCallback(
@@ -91,7 +105,7 @@ function useTouch(callback: (offset: number) => void) {
   );
 
   return {
-    isTouching: touchStartX !== null,
+    isTouching: touchStartX.current !== null,
     touchOffset,
     onTouchStart,
     onTouchMove,
