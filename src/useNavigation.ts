135d7c62b2a6a91c9a3b9698164e5887ab2a59a0
Alfred Ringstad <alfred.ringstad@hyperlab.se>
2019-09-17 11:13:28 +0200

    Fix totalSteps count

--- src/useNavigation.ts
+++ src/useNavigation.ts
@@ -31,7 +31,7 @@ function useNavigation({ infinite, slidesToShow, slideCount }: Options) {
       : currentIndex < 0
       ? currentIndex + slideCount
       : currentIndex;
-  const totalSteps = infinite ? slideCount : slideCount - slidesToShow;
+  const totalSteps = infinite ? slideCount : 1 + slideCount - slidesToShow;
 
   return {
     previous,
