eb6dd3d877107bcad53613feb85f53c9d1c25fa4
Alfred Ringstad <alfred.ringstad@hyperlab.se>
2021-12-17 14:43:49 +0100

    Upgrade packages and fix typeerrors

--- src/VerticalCarousel.tsx
+++ src/VerticalCarousel.tsx
@@ -25,109 +25,112 @@ export interface VerticalCarouselProps {
   render: (props: RenderProps) => React.ReactElement;
 }
 
-const VerticalCarousel: React.FC<VerticalCarouselProps> = (
-  {
-    children,
-    slidesToShow = 3,
-    infinite = true,
-    transitionDuration = 300,
-    render = ({ slides }: RenderProps) => slides
-  },
-  ref: () => void
-) => {
-  const { slides, slideCount, preSlidesCount } = useSlides(children, {
-    infinite,
-    slidesToShow,
-    mode: "flex"
-  });
-
-  const navigation = useNavigation({
-    infinite,
-    slidesToShow,
-    slideCount
-  });
-
-  const { previous, next, goToStep, currentIndex } = navigation;
-
-  React.useImperativeHandle(
-    ref,
-    () => ({
-      previous,
-      next,
-      goToStep
-    }),
-    [previous, next]
-  );
-
-  const wrapper = React.useRef(null);
-  const [itemSize, setItemSize] = React.useState(0);
-  const [disableTransition, setDisableTransition] = React.useState(false);
-
-  useItemSize({
-    wrapper,
-    callback: size => {
-      if (itemSize !== size) {
-        setDisableTransition(true);
-        setItemSize(size);
-      }
+export default React.forwardRef<
+  unknown,
+  React.PropsWithChildren<VerticalCarouselProps>
+>(
+  (
+    {
+      children,
+      slidesToShow = 3,
+      infinite = true,
+      transitionDuration = 300,
+      render = ({ slides }: RenderProps) => slides,
     },
-    slidesToShow,
-    measure: "height",
-    mode: "flex"
-  });
+    ref: () => void
+  ) => {
+    const { slides, slideCount, preSlidesCount } = useSlides(children, {
+      infinite,
+      slidesToShow,
+      mode: "flex",
+    });
 
-  React.useEffect(() => {
-    if (disableTransition) {
-      if (currentIndex < 0 || currentIndex >= slideCount) {
-        requestAnimationFrame(() =>
-          navigation.goToStep(index => {
-            if (index < 0) {
-              if (infinite) return slideCount + index;
-              return 0;
-            } else {
-              return index - slideCount;
-            }
-          })
-        );
-      } else {
-        requestAnimationFrame(() => setDisableTransition(false));
+    const navigation = useNavigation({
+      infinite,
+      slidesToShow,
+      slideCount,
+    });
+
+    const { previous, next, goToStep, currentIndex } = navigation;
+
+    React.useImperativeHandle(
+      ref,
+      () => ({
+        previous,
+        next,
+        goToStep,
+      }),
+      [previous, next]
+    );
+
+    const wrapper = React.useRef(null);
+    const [itemSize, setItemSize] = React.useState(0);
+    const [disableTransition, setDisableTransition] = React.useState(false);
+
+    useItemSize({
+      wrapper,
+      callback: (size) => {
+        if (itemSize !== size) {
+          setDisableTransition(true);
+          setItemSize(size);
+        }
+      },
+      slidesToShow,
+      measure: "height",
+      mode: "flex",
+    });
+
+    React.useEffect(() => {
+      if (disableTransition) {
+        if (currentIndex < 0 || currentIndex >= slideCount) {
+          requestAnimationFrame(() =>
+            navigation.goToStep((index) => {
+              if (index < 0) {
+                if (infinite) return slideCount + index;
+                return 0;
+              } else {
+                return index - slideCount;
+              }
+            })
+          );
+        } else {
+          requestAnimationFrame(() => setDisableTransition(false));
+        }
+      } else if (currentIndex >= slideCount || currentIndex < 0) {
+        setTimeout(() => setDisableTransition(true), transitionDuration);
       }
-    } else if (currentIndex >= slideCount || currentIndex < 0) {
-      setTimeout(() => setDisableTransition(true), transitionDuration);
-    }
-  }, [disableTransition, currentIndex, slideCount, transitionDuration]);
+    }, [disableTransition, currentIndex, slideCount, transitionDuration]);
 
-  const offset = -(currentIndex + preSlidesCount) * (100 / slidesToShow);
-  const transition = disableTransition || transitionDuration;
+    const offset = -(currentIndex + preSlidesCount) * (100 / slidesToShow);
+    const transition = disableTransition || transitionDuration;
 
-  return render({
-    ...navigation,
-    slides: (
-      <div
-        style={{
-          width: "auto",
-          height: "100%",
-          overflow: "hidden"
-        }}
-        ref={wrapper}
-      >
+    return render({
+      ...navigation,
+      slides: (
         <div
           style={{
-            display: "flex",
-            flexDirection: "column",
+            width: "auto",
             height: "100%",
-            transform: `translateY(${offset}%)`,
-            transition: `transform ${transition}ms ease`
+            overflow: "hidden",
           }}
+          ref={wrapper}
         >
-          {slides}
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              height: "100%",
+              transform: `translateY(${offset}%)`,
+              transition: `transform ${transition}ms ease`,
+            }}
+          >
+            {slides}
+          </div>
         </div>
-      </div>
-    ),
-    slidesToShow,
-    infinite,
-    transitionDuration
-  });
-};
-
-export default React.forwardRef(VerticalCarousel);
+      ),
+      slidesToShow,
+      infinite,
+      transitionDuration,
+    });
+  }
+);
